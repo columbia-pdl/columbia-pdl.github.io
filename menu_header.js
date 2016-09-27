9cb84ed800cf2759d22638d6f72081f8e724a2a0
Ellie Bastani <efb2123@columbia.edu>
2016-09-27 16:41:08 -0400

    Updates

---
+++ menu_header.js
@@ -0,0 +1,35 @@
+document.getElementById("navMenu").innerHTML =
+'<div class="nav-menu">'
++'<ul class="nav-menu-inner">'
+                        +'<li>'
+                            +'<a href="index.html">Home <i class="fa"></i></a>'
+                            
+                        +'</li>'
+                        +'<li>'
+                            +'<a href="events.html#events-build" class="">Communicate <i class=""></i></a>'
+                            
+                        +'</li>'
+                        +'<li>'
+                            +'<a href="events.html#events-design" class="">Lead <i class="fa"></i></a>'
+                        +'</li>'
+                        +'<li>'
+                            +'<a href="events.html#events-sync" class="">Collaborate<i class="fa"></i></a>'
+                            
+                        +'</li>'
+                        +'<li>'
+                            +'<a href="events.html#events-all">Innovate<i class="fa"></i></a>'
+                            
+                        +'</li>'
+                        +'<li>'
+                            +'<a href="events.html#events-network" class="">Connect<i class="fa"></i></a>'
+                            
+                        +'</li>'
+                        +'<li>'
+                            +'<a href="events.html#events-all">Tech<i class="fa"></i></a>'
+                            
+                        +'</li>'
+                        +'<li>'
+                           +'<a href="contact.html">Contact<i class="fa"></i></a>'
+                        +'</li>'
+                    +'</ul>'
+                    +'</div>';
