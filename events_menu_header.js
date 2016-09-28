ccef0e26e57a39554e63072f5b690afc93840a15
Ellie Bastani <efb2123@columbia.edu>
2016-09-28 17:58:25 -0400

    update links

---
+++ events_menu_header.js
@@ -0,0 +1,37 @@
+document.getElementById("navMenu").innerHTML =
+'<div class="nav-menu">'
++'\t<ul class="nav-menu-inner">'
+                       +'\t\t<li>\n'
+                       +'\t\t\t<a onclick="window.location.href=\'events.html#events-communicate\';onPageLoad()"><i class="ion ion-ios-compose-outline"></i>Communicate</a>\n' 
+                       +'\t\t</li>\n\n'
+
+                       +'\t\t<li>\n'
+                       +'\t\t\t<a onclick="window.location.href=\'events.html#events-lead\';onPageLoad()"><i class="ion ion-ios-compose-outline"></i>Lead</a>\n' 
+                       +'\t\t</li>\n\n'
+
+                       +'\t\t<li>\n'
+                       +'\t\t\t<a onclick="window.location.href=\'events.html#events-collaborate\';onPageLoad()"><i class="ion ion-ios-compose-outline"></i>Collaborate</a>\n' 
+                       +'\t\t</li>\n\n'
+
+                       +'\t\t<li>\n'
+                       +'\t\t\t<a onclick="window.location.href=\'events.html#events-innovate\';onPageLoad()"><i class="ion ion-ios-compose-outline"></i>Innovate</a>\n' 
+                       +'\t\t</li>\n\n'
+
+                       +'\t\t<li>\n'
+                       +'\t\t\t<a onclick="window.location.href=\'events.html#events-connect\';onPageLoad()"><i class="ion ion-ios-compose-outline"></i>Connect</a>\n' 
+                       +'\t\t</li>\n\n'
+
+                       +'\t\t<li>\n'
+                       +'\t\t\t<a onclick="window.location.href=\'events.html#events-tech\';onPageLoad()"><i class="ion ion-ios-compose-outline"></i>Tech</a>\n' 
+                       +'\t\t</li>\n\n'
+
+                       +'\t\t<li>\n'
+                       +'\t\t\t<a href="index.html"<i class="ion ion-ios-home-outline"></i></a>\n'   
+                       +'\t\t</li>\n\n'
+
+                       +'\t\t<li>\n'
+                       +'\t\t\t<a href="contact.html"<i class="ion ion-ios-email-outline"></i></a>\n'   
+                       +'\t\t</li>\n\n'
+
+                    +'\t</ul>'
+                    +'</div>';
