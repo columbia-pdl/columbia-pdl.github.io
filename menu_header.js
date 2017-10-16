5391d989b538b428f4a4a1d4df3d6d2945166301
Ellie Bastani <efb2123@columbia.edu>
2017-10-16 17:24:56 -0400

    Updates to Landing page

--- menu_header.js
+++ menu_header.js
@@ -2,27 +2,30 @@ document.getElementById("navMenu").innerHTML =
 '<div class="nav-menu">'
 +'<ul class="nav-menu-inner">'
                         +'<li>'
-                        +'<a href="events.html#events-communicate"><i class="ion ion-ios-compose-outline"></i>Communicate</a>'
+                        +'<a href="events.html#events-communicate"><i class="ion ion-ios-compose-outline"></i></a>'
                             
                         +'</li>'
                         +'<li>'
-                            +'<a href="events.html#events-lead"><i class="ion ion-ios-person-outline"></i>Lead</a>'
+                            +'<a href="events.html#events-lead"><i class="ion ion-ios-people-outline"></i></a>'
                             
                         +'</li>'
                         +'<li>'
-                            +'<a href="events.html#events-collaborate" class=""><i class="ion ion-ios-people-outline"></i>Collaborate</a>'
+                            +'<a href="events.html#events-collaborate" class=""><i class="ion ion-ios-briefcase-outline"></i></a>'
                         +'</li>'
                         +'<li>'
-                            +'<a href="events.html#events-innovate" class=""><i class="ion ion-ios-lightbulb-outline"></i>Innovate</a>'
+                            +'<a href="events.html#events-innovate" class=""><i class="ion ion-ios-bookmarks-outline"></i></a>'
                             
                         +'</li>'
                         +'<li>'
-                            +'<a href="events.html#events-connect"><i class="ion ion-ios-color-filter-outline"></i>Connect</a>'
+                            +'<a href="events.html#events-connect"><i class="ion ion-ios-color-filter-outline"></i></a>'
                             
                         +'</li>'
                         +'<li>'
-                            +'<a href="events.html#events-tech" class=""><i class="ion ion-ios-gear-outline"></i>Tech</i></a>'
-                            
+                            +'<a href="events.html#events-tech" class=""><i class="ion ion-compass"></i></i></a>'
+                        
+                        +'</li>'
+                        +'<li>'
+                            +'<a href="events.html#events-tech" class=""><i class="ion ion-ios-plus-outline"></i></i></a>'    
                         +'</li>'
                         +'<li>'
                             +'<a href="index.html"<i class="ion ion-ios-home-outline"></i></a>'
